99a8017590ba3d219b07a2b47e31f32bf847ecdf
ceh <dj123456>
2020-01-07 17:14:12 +0800

    hook 80%

--- hook_dll/InlineHook.cpp
+++ hook_dll/InlineHook.cpp
@@ -1,50 +1,81 @@
 #include"InlineHook.h"
 #include "pch.h"
 
-#define HOOK_ORIADDR  0x004A6F7C
+#define HOOK_ORIADDR  0x004A6F7C //钩子起始地址
 #define PATCH_LENGTH  8
 DWORD dwHookAddr;
+BYTE  byCall[PATCH_LENGTH];
+BYTE  oldCall[PATCH_LENGTH];
 DWORD dwRetAddr;
-//钩子函数
-void _declspec (naked)  HookFunc() {
+DWORD  dwOldProtect;
 
-	_asm {
-		//1 保存寄存器
-		pushad
-		pushfd
+DWORD c = 0x004052BC;//内联汇编跳转的call 地址
+
+char  szNewCode[] = "7e321-00000-d41d8-00000";
 
 
 
 
 
-		popfd
-		popad
+void setCode() {
 
-	}
 
 
 }
+
+
+
+
+//钩子函数
+void _declspec (naked)  HookFunc() {
+	__asm {
+		//保存寄存器
+		// 修改数据
+		lea eax, DWORD PTR DS:[szNewCode];
+		mov dword ptr ss : [ebp - 0x4] , eax;
+		//恢复寄存器
+		mov edx, dword ptr ss : [ebp - 0x4]
+		call c;
+		jmp dwRetAddr;
+	}
+}
+
+void UnHook() {
+	//恢复原先字节
+	memcpy((void*)dwHookAddr, oldCall, PATCH_LENGTH);
+	//恢复属性
+	DWORD p;
+	VirtualProtect((void*)dwHookAddr, PATCH_LENGTH, dwOldProtect, &p);
+}
+
+
 	//下钩函数
 	void  HookFun() {
-		//要hook的地址
-		dwHookAddr = HOOK_ORIADDR;
-		 //ret 返回的地址
-		dwRetAddr = dwHookAddr+ PATCH_LENGTH;
-		//初始化地址
-		BYTE  byJmpCall[PATCH_LENGTH] = {0xE9};
-		DWORD  dwOldProtect = 0;
-		memset(&byJmpCall[1],0x90, PATCH_LENGTH - 1);
-		memset(&byJmpCall[2], 0x90, PATCH_LENGTH - 2);
-		memset(&byJmpCall[3], 0x90, PATCH_LENGTH - 3);
-		//存储跳转地址
-		*(DWORD*)&byJmpCall[1]=(DWORD)HookFunc - (DWORD)dwHookAddr - 5;
-		//备份被之前覆盖的数据
+			//要hook的地址
+			dwHookAddr = HOOK_ORIADDR;
+			// //ret 返回的地址
+			dwRetAddr = dwHookAddr + PATCH_LENGTH;
+			//初始化地址
+			byCall[0] = { 0xE9 };
+			dwOldProtect = 0;
 
-		//修改内存属性
-		VirtualProtect((LPVOID)HOOK_ORIADDR, PATCH_LENGTH, PAGE_EXECUTE_READWRITE,&dwOldProtect);
-		memcpy((LPVOID)dwHookAddr, byJmpCall, PATCH_LENGTH);
-	
-	}
+			//设置后面的为nop 
+			memset(&byCall[1], 0x90, PATCH_LENGTH - 1);
+
+
+			//存储跳转地址 要跳转的地址 -  当前地址 - 5
+			*(DWORD*)&byCall[1] = (DWORD)HookFunc - (DWORD)dwHookAddr - 5;
+
+			// 一般代码段是不可写的,我们需要把其改为可读可写.
+			VirtualProtect((LPVOID)HOOK_ORIADDR, PATCH_LENGTH, PAGE_EXECUTE_READWRITE, &dwOldProtect);
+			//保存原先字节
+			memcpy(oldCall, (void*)dwHookAddr, PATCH_LENGTH);
+
+			//替换原先字节
+			memcpy((void*)dwHookAddr, byCall, PATCH_LENGTH);
+			// 恢复钩子
+			//UnHook();
+	}	
 
 
 	void SetHook() {
