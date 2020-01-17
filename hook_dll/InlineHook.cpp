034e5c774cbc823478db1f7cecb231603dd2b306
ceh <dj123456>
2020-01-17 15:45:17 +0800

    修改hook dll

--- hook_dll/InlineHook.cpp
+++ hook_dll/InlineHook.cpp
@@ -2,66 +2,59 @@
 #include "pch.h"
 #include<iostream>
 
-#define HOOK_ORIADDR  0x004A6F7C //钩子起始地址
-#define PATCH_LENGTH  8      
-DWORD dwHookAddr;
-DWORD dwRetAddr;
-BYTE  byCall[PATCH_LENGTH];
-BYTE  oldCall[PATCH_LENGTH];
+#define HOOK_ORIADDR  0x004A3AB8 //钩子起始地址
+
+
+#define PATCH_LENGTH  6     
+
+DWORD dwHookAddr; //hook地址
+
+DWORD dwRetAddr; //hook返回地址
+
+DWORD dwHookAppendAddr;
+
+DWORD dwRetAppendAddr;
+
+BYTE  byCall[PATCH_LENGTH]; // jmp 机器码
+
+BYTE  oldCall[PATCH_LENGTH]; //卸载 机器码
+
+
 DWORD  dwOldProtect;
-
-DWORD  dwCallAddressVp = 0x004A6F58; //判断是否是这个地址调的call
-
-DWORD c = 0x004052BC;//内联汇编跳转的call 地址
 
 char  szNewCode[] = "ba855-00000-88aa6-00000";
 
 
 
+void  HookAppend();
 
 
-void setCode() {
 
 
-
-}
-//瞬时钩子
-void _declspec (naked)  HookFunAppend() {
-	__asm {
-		//保存寄存器
-		pushad;
-		pushfd;
-		mov eax, dword ptr ss : [esp - 0x4];
-		cmp eax, dwCallAddressVp;
-		jnz Lable;
-		//做点啥
-
-		//恢复寄存器
-		popfd;
-		popad;
-
-	Lable:
-		popfd;
-		popad;
-	}
+void saveValue(DWORD v) {
+	MessageBox(0, (LPCSTR)v,0,0);
 }
 
+DWORD pEax = 0;
+//_declspec (naked)什么是一个裸函数 就是不做任何其他多余的操作
 //钩子函数
 void _declspec (naked)  HookFunc() {
 	__asm {
-		//保存寄存器
-		pushad;
-		pushfd;
-		// 修改数据
-		lea eax, DWORD PTR DS:[szNewCode];
-		mov dword ptr ss : [ebp - 0x4] , eax;
-		//恢复寄存器
-		popfd;
-		popad;
-		mov edx, dword ptr ss : [ebp - 0x4] ;
-		call c;
-		jmp dwRetAddr;
+		mov eax, dword ptr ss : [ebp - 0x2C4]
+		mov dword ptr ss : [ebp - 0x2A8] , eax
+		pushad
+		pushfd
+		mov pEax, eax
 	}
+	saveValue(pEax);
+	__asm {
+		popfd
+		popad
+		jmp dwRetAddr
+	}
+
+
+
 }
 
 void UnHook() {
@@ -85,25 +78,37 @@ void UnHook() {
 			//设置后面的为nop 
 			memset(&byCall[1], 0x90, PATCH_LENGTH - 1);
 
-
 			//存储跳转地址 要跳转的地址 -  当前地址 - 5
 			*(DWORD*)&byCall[1] = (DWORD)HookFunc - (DWORD)dwHookAddr - 5;
-
+				
 			// 一般代码段是不可写的,我们需要把其改为可读可写.
 			VirtualProtect((LPVOID)dwHookAddr, PATCH_LENGTH, PAGE_EXECUTE_READWRITE, &dwOldProtect);
-			
-			//保存原先字节
-			memcpy(oldCall, (void*)dwHookAddr, PATCH_LENGTH);
+			//获取自己进程的句柄
+			HANDLE handle=OpenProcess(PROCESS_ALL_ACCESS,NULL,GetCurrentProcessId());
+				
+			//备份hook地址
+			BOOL saveFlg=ReadProcessMemory(handle, (LPCVOID)dwHookAddr, oldCall, PATCH_LENGTH,NULL);
+			if (!saveFlg) {
+				MessageBox(0,"备份hook地址失败",0,0);
+				return;
+			}
 
 			//替换原先字节  替换成 jmp e9 00000000
-			memcpy((void*)dwHookAddr, byCall, PATCH_LENGTH);
-	}	
-		
-	void  HookAppend() {
-	
-	
-	
+			BOOL writeFlg=WriteProcessMemory(handle, (LPVOID)dwHookAddr, byCall, PATCH_LENGTH,NULL);
+			if (!writeFlg) {
+				MessageBox(0, "备份hook写入失败", 0, 0);
+				return;
+			}
 	}
+
+	//瞬时下钩函数 未实现
+	void  HookAppend() {
+		
+	}
+	
+
+
+
 	//普通钩子
 	void SetHook() {
 
@@ -111,6 +116,5 @@ void UnHook() {
 	}
 	//瞬时钩子
 	void SetHookAppend() {
-
 		HookAppend();
 	}
